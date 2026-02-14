2f1fba48720bfba93b47b888848fa4a2affbbd43
Faheemah Mohamed <faheemahmohamed6@gmail.com>
2026-02-14 22:10:46 +0300

    for all my input values with there specific id's this code gets what the user typed and removes extra spaces

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 //wait until page fully loads
-
 document.addEventListener("DOMContentLoaded",function() {
-
+//Find form and store in a variable
  const form = document.getElementById("reservationForm");
-
+//When someone click submit button,run this function
  if(form){
  form.addEventListener("submit",function(e) {
+//stops normal behaviour
 e.preventDefault();
+//get input values
+const name = document.getElementById("name").ariaValueMax.trim();
+const phone = document.getElementById("number").value.trim();
+const email = document.getElementById("email").value.trim();
+const date = document.getElementById("date").value.trim();
+const time = document.getElementById("time").value;
+const guests = document.getElementById("guests").value;
+
+
 }
  }
